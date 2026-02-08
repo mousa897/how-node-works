1ac67455d424f0ebceb6eb88bc5af09968df6cab
Mousa <mousaalihamadna@gmail.com>
2026-02-08 15:11:43 +0300

    first commit

---
+++ event-loop.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+const start = Date.now();
+
+// Understanding the EventLoop and threadpool
+
+setTimeout(() => console.log("Timer 1 finished"), 0);
+setImmediate(() => console.log("Immediate 1 finished"));
+
+fs.readFile("test-file.txt", () => {
+  console.log("I/0 finished");
+  console.log("---------------");
+
+  setTimeout(() => console.log("Timer 2 finished"), 0);
+  setTimeout(() => console.log("Timer 3 finished"), 3000);
+  setImmediate(() => console.log("Immediate 2 finished"));
+
+  process.nextTick(() => console.log("Process.nextTick"));
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted");
+  });
+});
+
+console.log("hello from top level code");
